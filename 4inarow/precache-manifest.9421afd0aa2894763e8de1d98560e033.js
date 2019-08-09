992eeb894783c000b5d5d1092de9ba2336210bb7
oi <onno.invernizzi@ivention.nl>
2019-08-09 14:11:50 +0200

    Added 4 in a row

---
+++ 4inarow/precache-manifest.9421afd0aa2894763e8de1d98560e033.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb77a1f36fdf0648cc0bb4041ecfe02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26acc32e7377dad1cf60",
+    "url": "/static/css/main.cc535bf0.chunk.css"
+  },
+  {
+    "revision": "10bf244ca692441702db",
+    "url": "/static/js/2.1b3a0643.chunk.js"
+  },
+  {
+    "revision": "26acc32e7377dad1cf60",
+    "url": "/static/js/main.962242ac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
