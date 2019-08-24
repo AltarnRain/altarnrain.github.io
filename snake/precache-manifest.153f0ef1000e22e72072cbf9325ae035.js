0bc40260b22181d30a1eae48fd63022c462a6908
oi <onno.invernizzi@ivention.nl>
2019-08-25 00:20:07 +0200

    Added snake

---
+++ snake/precache-manifest.153f0ef1000e22e72072cbf9325ae035.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a85f09a96c2cef4c2bd942640852f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bcac99207fa9854aca5",
+    "url": "/static/js/2.06822bfd.chunk.js"
+  },
+  {
+    "revision": "85c200fcff0f4af82360",
+    "url": "/static/js/main.44f02d51.chunk.js"
+  },
+  {
+    "revision": "8ff2ab152d0af3130c6a",
+    "url": "/static/js/runtime~main.c153aad1.js"
+  }
+]);
