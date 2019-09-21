971e500d6203d6e424c9c0f8179fbf5c8faf68bd
oi <onno.invernizzi@ivention.nl>
2019-09-21 17:59:48 +0200

    Breakout update

---
+++ breakout/precache-manifest.5e9ab603dc259ad074feed04627b7a6a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e91a4d2603ec544c9133ffc721e965",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10b4d848a4b6ebed3217",
+    "url": "/static/js/2.da68095c.chunk.js"
+  },
+  {
+    "revision": "10c41d755bcc859da7e0",
+    "url": "/static/js/main.f42dddb6.chunk.js"
+  },
+  {
+    "revision": "86e08dcd64e03754d750",
+    "url": "/static/js/runtime~main.5d566055.js"
+  }
+]);
